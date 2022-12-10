fa9c3e604ad390958d5c328f481af9ed0c02733d
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-12-09 21:37:48 -0300

    ex 1041

--- ex1041.js
+++ ex1041.js
@@ -1,21 +1,25 @@
 var input = require('fs').readFileSync('./stdin', 'utf8');//2.0 4.0 7.5 8.0
 var lines = input.split('\n');//[ '2.0 4.0 7.5 8.0' ]
 const dados = lines[0].split(' ')
-const x = Number(dados[0])
-const y = Number(dados[1])
+const x = Number(parseFloat(dados[0]).toFixed(1))
+const y = Number(parseFloat(dados[1]).toFixed(1))
 
-if(x == 0 && y == 0){
-   return 'Origem'
-}else if(x > 0 ){
-    if(y > 0 ){
-        return 'Q1' 
-    }else if(y < 0){
-        return 'Q4'
-    }
-}else if(x < 0){
-    if(y > 0 ){
-        return 'Q2'
-    }else if(y < 0){
-        return 'Q3'
-    }
+
+  if(x == 0 && y == 0){
+console.log(`Origem`) 
+}else if(x != 0 && y == 0){
+console.log(`Eixo X`)  
+}else if(x == 0 && y!= 0){
+console.log(`Eixo Y`)  
+}else if(x > 0 && y > 0){
+console.log(`Q1`)  
+}else if(x > 0 && y < 0){
+console.log(`Q4`) 
+}else if(x < 0 && y > 0){
+console.log(`Q2`)  
+}else if(x < 0 && y < 0){
+console.log(`Q4`)  
 }
+
+
+
